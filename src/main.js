6e471e64734622dd235def8164ef04e407e85b39
kubanychbek <kushtarbekovkuba@gmail.com>
2022-05-30 22:31:24 +0600

    .

--- src/main.js
+++ src/main.js
@@ -3,14 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-const cors = require('cors');
-const corsConfig = {
-  credentials: true,
-  origin: true,
-};
-
 const app = createApp(App);
 app.use(store);
 app.use(router);
-app.use(cors(corsConfig));
 app.mount("#app");
