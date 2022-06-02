7bdb26704610ab16117d6c0aca88ee5f1687e4c9
Алакен Кубатбеков <kubatbekovalaken@gmail.com>
2022-06-02 14:22:58 +0600

    depl

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,10 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: "./",
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8083',
+        target: 'http://192.168.50.26:8083',
         pathRewrite: { '^/api': '' },
       },
     },
